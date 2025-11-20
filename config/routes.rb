b57e4eb9f190c594c98bd7137218d96f190b0f57
Ayesha Khan <bsef20a007@pucit.edu.pk>
2025-11-20 12:01:35 +0000

    Added the route of projects

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,25 @@ Rails.application.routes.draw do
   get "registrations/new_ceo"
   get "registrations/create_ceo"
 
+  # Defines the root path
+  root "pages#home"
+  get "pages/home"
+
   #skip default route of devise gem 
   devise_for :users, skip: [ :registrations ]
 
   get  "/ceo_signup", to: "registrations#new_ceo",    as: :new_ceo_signup
   post "/ceo_signup", to: "registrations#create_ceo", as: :create_ceo_signup
+  
   # login path
   match "/ceo_login", to: "registrations#login_ceo", via: [ :get, :post ], as: :login_ceo
-
 
   # Company profile creation (after signup)
   resources :companies, only: [ :new, :create, :show ]
 
-  # Defines the root path
-  root "pages#home"
+  #projects maagement
+  resources :projects
 
-  get "pages/home"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
