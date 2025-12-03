96cc59d2933ffb596f2269daf4a20781caf51455
Ayesha Khan <bsef20a007@pucit.edu.pk>
2025-12-03 08:12:01 +0000

    Added username and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,16 @@ class User < ApplicationRecord
   #added for email check now
   REGEX_EMAIL = /\A[^@\s]+@[^@\s]+\z/
 
+  #email validations 
+  validates :email, format: { with: REGEX_EMAIL, message: "Invalid Email Format."}
+  validates :email, length: {maximum: 50, message: "Too Long Email. (Max Allow 50 Characters)"}   #in form do not add chars more than 50
+
+  # username validation (to resolve the unique index err)
+  validates :username, uniqueness: { case_sensitive: false, message: "Username already taken" }
+
   ROLES = %W[admin manager employee client super_admin]
 
   #added the metaprogramming dynamically method definition
-  
   ROLES.each do |type|
     define_method "#{type}?" do
       role == type
