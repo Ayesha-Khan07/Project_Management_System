2ec8d32beecf73e37787d78bbf5983b55ba309f3
Ayesha Khan <bsef20a007@pucit.edu.pk>
2025-12-08 12:17:32 +0000

    Resolve the issue of updated_by  by paper trails whodunnit.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,9 @@ class Project < ApplicationRecord
   belongs_to :manager, class_name: "User", foreign_key: "manager_id", optional: true
   belongs_to :client, class_name: "User", foreign_key: "client_id", optional: true
 
-  # has_rich_text :description
+  # paper trail 
+  has_paper_trail
+
   has_one_attached :uploaded_document
 
     #join table association
