07f11bc1f3c0a024e755d8fe6a5ae94628017f0c
Ayesha Khan <bsef20a007@pucit.edu.pk>
2025-12-09 13:26:06 +0000

    Implement the mail notification functionality on comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,10 @@ class CommentsController < ApplicationController
     @comment.user = current_user
 
     if @comment.save
+      #send email seperatly to each associated user ---> becoz bcc won't works 
+       @task.project.users.where.not(id: current_user.id).find_each do |recipient|
+        CommentMailer.with(comment: @comment, action: "created", recipient: recipient).comment_notification.deliver_now
+      end
       redirect_to project_task_path(@task.project, @task), notice: "Comment added."
     else
       redirect_to project_task_path(@task.project, @task), alert: "Comment cannot be blank."
@@ -22,6 +26,11 @@ class CommentsController < ApplicationController
     PaperTrail.request.whodunnit = current_user.id
 
     if @comment.user == current_user && @comment.update(comment_params)
+      
+      @task.project.users.where.not(id: current_user.id).find_each do |recipient|
+        CommentMailer.with(comment: @comment, action: "updated", recipient: recipient).comment_notification.deliver_now
+      end
+
       redirect_to project_task_path(@task.project, @task), notice: "Comment updated."
     else
       redirect_to project_task_path(@task.project, @task), alert: "Not allowed or blank content."
@@ -31,6 +40,11 @@ class CommentsController < ApplicationController
   def destroy
     if @comment.user == current_user
       @comment.update(body: "This comment was deleted.")
+      #send mail before redirect
+      @task.project.users.where.not(id: current_user.id).find_each do |recipient|
+        CommentMailer.with(comment: @comment, action: "deleted", recipient: recipient).comment_notification.deliver_now
+      end
+
       redirect_to project_task_path(@task.project, @task), notice: "Comment deleted."
     else
       redirect_to project_task_path(@task.project, @task), alert: "Not allowed."
